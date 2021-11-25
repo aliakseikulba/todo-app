22ebc4ebcef7cbc04afdae001eceeb8ee6a7292d
aliakseikulba <kulba.av@gmail.com>
2021-11-25 20:39:48 +0300

    add reduser tests

--- src/store/reducer.test.ts
+++ src/store/reducer.test.ts
@@ -17,7 +17,7 @@ test('sum should be right', () => {
 });
 
 
-test('result of calculating should be right', () => {
+test('result of sum should be right', () => {
 
   const state = 10;
 
@@ -28,4 +28,57 @@ test('result of calculating should be right', () => {
   const result = calculator(state, action);
 
   expect(result).toBe(30);
+})
+
+test('result of multiply should be right', () => {
+
+  const state = 20;
+
+  const action: ActionType = {
+    type: 'MUL',
+    number: 20
+  }
+  const result = calculator(state, action);
+
+  expect(result).toBe(400);
+})
+
+test('result of subtract should be right', () => {
+
+  const state = 50;
+
+  const action: ActionType = {
+    type: 'SUB',
+    number: 20
+  }
+  const result = calculator(state, action);
+
+  expect(result).toBe(30);
+})
+
+test('result of dividing should be right', () => {
+
+  const state = 100;
+  const action: ActionType = {
+    type: 'DIV',
+    number: 20
+  }
+
+  const result = calculator(state, action);
+
+  expect(result).toBe(5);
+})
+
+
+test('result of exponentiation should be right', () => {
+
+  const state = 10;
+
+  const action: ActionType = {
+    type: 'EXP',
+    number: 2
+  }
+  const result = calculator(state, action);
+
+  expect(result).toBe(100);
 })
