5293fa05d7100b5d5b574811bf7979a0063f69cd
aliakseikulba <kulba.av@gmail.com>
2022-01-05 13:41:20 +0300

    connect provider with App.stories

--- src/stories/App.stories.tsx
+++ src/stories/App.stories.tsx
@@ -1,18 +1,15 @@
 import React from 'react';
 import {ComponentMeta, ComponentStory} from '@storybook/react';
 import {App} from '../App';
-import {Provider} from 'react-redux';
-import {store} from '../state/store';
+import {ReduxStoreProviderDecorator} from './decorators/ReduxStoreProviderDecorator';
 
 export default {
   title: 'TodoList/App',
   component: App,
-  args: {
-
-  },
+  decorators: [ReduxStoreProviderDecorator],
 } as ComponentMeta<typeof App>;
 
-const Template: ComponentStory<typeof App> = (args) => <Provider store={store}> <App/></Provider>;
+const Template: ComponentStory<typeof App> = (args) => <App/>;
 
 export const AppStory = Template.bind({});
 AppStory.args = {
