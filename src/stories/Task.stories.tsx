5de0fe3e464fae641f4063d98d5afb2d47bb557f
aliakseikulba <kulba.av@gmail.com>
2022-01-05 12:43:50 +0300

    fix args

--- src/stories/Task.stories.tsx
+++ src/stories/Task.stories.tsx
@@ -6,24 +6,21 @@ import {action} from '@storybook/addon-actions';
 export default {
   title: 'TodoList/Task',
   component: Task,
+  args: {
+    removeTask: action('Remove button inside Task'),
+    changeTaskStatus: action('Status changed inside Task'),
+    changeTaskTitle: action('Title changed inside Task'),
+  },
 } as ComponentMeta<typeof Task>;
-
-const baseArgs = {
-  removeTask: action('Remove button inside Task'),
-  changeTaskStatus: action('Status changed inside Task'),
-  changeTaskTitle: action('Title changed inside Task'),
-}
 
 const Template: ComponentStory<typeof Task> = (args) => <Task {...args} />;
 
 export const TaskIsDoneStory = Template.bind({});
 TaskIsDoneStory.args = {
-  ...baseArgs,
   task: {id: '1', isDone: true, title: 'JS' },
 };
 
 export const TaskIsNotDoneStory = Template.bind({});
 TaskIsNotDoneStory.args = {
-  ...baseArgs,
   task: {id: '2', isDone: false, title: 'TS' },
 };
