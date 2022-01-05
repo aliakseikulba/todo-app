989a50f2aac9c05398106f824cdbaedcf4b13983
aliakseikulba <kulba.av@gmail.com>
2022-01-05 12:20:41 +0300

    fix Stories

--- src/stories/AddItemForm.stories.tsx
+++ src/stories/AddItemForm.stories.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import {ComponentMeta, Story} from '@storybook/react';
-import {AddItemForm, AddItemFormPropsType} from '../AddItemForm';
+import {ComponentMeta, ComponentStory} from '@storybook/react';
+import {AddItemForm} from '../AddItemForm';
 import {action} from '@storybook/addon-actions';
 
 export default {
@@ -8,7 +8,7 @@ export default {
   component: AddItemForm,
 } as ComponentMeta<typeof AddItemForm>;
 
-const Template: Story<AddItemFormPropsType> = (args) => <AddItemForm {...args} />;
+const Template: ComponentStory<typeof AddItemForm> = (args) => <AddItemForm {...args} />;
 
 export const AddItemFormStory = Template.bind({});
 AddItemFormStory.args = {
--- src/stories/Task.stories.tsx
+++ src/stories/Task.stories.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import {ComponentMeta, Story} from '@storybook/react';
-import {Task, TaskPropsType} from '../Task';
+import {ComponentMeta, ComponentStory} from '@storybook/react';
+import {Task} from '../Task';
 import {action} from '@storybook/addon-actions';
 
 export default {
@@ -12,7 +12,7 @@ const removeTaskCallback = action('Remove button inside Task');
 const changeTaskStatusCallback = action('Status changed inside Task');
 const changeTaskTitleCallback = action('Title changed inside Task');
 
-const Template: Story<TaskPropsType> = (args) => <Task {...args} />;
+const Template: ComponentStory<typeof Task> = (args) => <Task {...args} />;
 
 const baseArgs = {
   removeTask: removeTaskCallback,
